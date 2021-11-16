938ff27d2fff9a5fd01e5d68b36ef7c1d6fb5ffa
Gustavo Fernandes <gustavomarcelinofernandes25@gmail.com>
2021-11-16 15:29:43 -0300

    initial commit app

---
+++ src/pages/home/home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View
+} from 'react-native';
+
+
+
+export default function Home() {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Home</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
